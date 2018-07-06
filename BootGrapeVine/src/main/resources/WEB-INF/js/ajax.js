b1247e76ad2601db3b6ad9efe9d4c181a2a1f028
AshGale <ashgale17@yahoo.co.uk>
2018-07-06 17:18:20 +0100

    added some js gets

--- BootGrapeVine/src/main/resources/WEB-INF/js/ajax.js
+++ BootGrapeVine/src/main/resources/WEB-INF/js/ajax.js
@@ -1,14 +1,42 @@
+//			for non thymleaf 
+// 			var token = $("meta[name='_csrf']").attr("content");
+// 			var header = $("meta[name='_csrf_header']").attr("content");
+// 			$(document).ajaxSend(function(e, xhr, options) {
+// 				xhr.setRequestHeader(header, token);
+// 			});
 
-function something(){
-	app.alert("hello");
+function queryParamiters(url) {
+	// get filled paramiters from Form
+	console.log("queryParamiters");
+	let myForm = document.getElementById('form-knockknock');
+	let allInputs = myForm.getElementsByTagName('input');
+	url += '?'; // start query paramiter section;
+	for (let i = 0; i < allInputs.length; i++) { // TODO add foreach $each;
+		let input = allInputs[i];
+		//if (input.value != "") {
+			url += input.id + '=' + input.value + '&';
+		//}
+	}
+	url.slice(0, -1); // tody last & character;
+	console.log(url);
+	return url;
+}
+
+function submitKnockknock(){
+	event.preventDefault();
+	 console.log("submitknockknock");
+	var url = queryParamiters("http://localhost:8080/knockknock");
+	$.ajax({
+		url : url,
+		type : "GET"
+	}).then(function(data) {
+		console.log("then funciton");
+		//document.getElementsByClassName("person").innerHTML = "";
+		$('.person-id').text(data.id);
+		$('.person-Name').text("Hello " + data.firstName + " " + data.lastName);
+	});
 }
 
 $(document).ready(function() {
-    $.ajax({
-        url: "http://localhost:8080/knockknock"
-    }).then(function(data) {
-       $('.person-id').append(data.id);
-       $('.person-firstName').append(data.firstName);
-       $('.person-lastName').append(data.lastName);
-    });
+
 });
